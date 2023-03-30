5add86a1185aad36d3c22e75b28694deb71c3dba
Ruchi Mandliya <rmandliya18@gmail.com>
2023-03-31 00:21:04 +0530

    add textfields

--- react-quiz-app/src/pages/home/Home.js
+++ react-quiz-app/src/pages/home/Home.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import './Home.css';
+import {TextField} from '@mui/material'
 
 const Home = () => {
   return (
     <div className='content'>
       <div className='settings'>
         <span style={{fontSize: 30}}>Quiz Settings</span>
+        <div className='settings__select'>
+          <TextField style={{marginBottom:25}} label='Enter Your Name' variant='outlined'
+
+          />
+          <TextField
+          select label='Select Category'
+          variant='outlined'
+          style={{marginBottom: 30}}/>
+
+        </div>
  
       </div>
       <img src='/think.svg' className='banner' alt = "quiz img"/>
+
     </div>
   )
 }
