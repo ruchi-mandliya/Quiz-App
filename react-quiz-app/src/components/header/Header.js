aaccc5bab737c1353f4d4f41e0b3ec0c98207cac
Ruchi Mandliya <rmandliya18@gmail.com>
2023-03-30 17:01:36 +0530

    add background

--- react-quiz-app/src/components/header/Header.js
+++ react-quiz-app/src/components/header/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='header'>
-        <Link to = "/" className='title'>Intutive Quiz Hub
-        </Link>
-        <hr className='divider'/>
+    <div>
+      Header
+        
+       
     </div>
   )
 }
