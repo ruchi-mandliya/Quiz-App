8870dbdc14a76d6faeea96103918816df19bd158
Ruchi Mandliya <rmandliya18@gmail.com>
2023-03-30 23:53:50 +0530

    add image on homepage

--- react-quiz-app/src/pages/home/Home.js
+++ react-quiz-app/src/pages/home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='content'>
+      <div className='settings'>
+        <span style={{fontSize: 30}}>Quiz Settings</span>
+ 
+      </div>
+      <img src='/think.svg' className='banner' alt = "quiz img"/>
+    </div>
   )
 }
 
