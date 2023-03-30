80b104ba5f2cf721de2ff07fd93d0d8845b56de9
Ruchi Mandliya <rmandliya18@gmail.com>
2023-03-30 17:11:17 +0530

    add hr tag

--- react-quiz-app/src/App.js
+++ react-quiz-app/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- react-quiz-app/src/components/header/Header.js
+++ react-quiz-app/src/components/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './Header.css';
 
 const Header = () => {
   return (
     <div>
-      Header
+      <Link to = '/'>Quiz Hub</Link>
+      <hr/>
         
        
     </div>
