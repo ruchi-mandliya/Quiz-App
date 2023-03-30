bb2448649d822b821e8e500f2c7dbe2909dff062
Ruchi Mandliya <rmandliya18@gmail.com>
2023-03-30 17:43:58 +0530

    add routing using react-router-dom

--- react-quiz-app/src/App.js
+++ react-quiz-app/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Quiz from "./pages/quiz/Quiz";
+import Result from "./pages/result/Result";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
         <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/Quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
